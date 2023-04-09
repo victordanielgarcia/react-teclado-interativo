99b41d0adf8f7d2400afd09ebb0df550c4acfa15
Victor Daniel <victordanigarcia20@gmail.com>
2023-04-09 08:01:07 -0300

    Adicionado todas as funcionalidades do teclado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,161 @@
-import './index.css';
-import React from 'react';
+import './index.scss';
+import { keyboard } from './keys';
+import * as React from 'react';
 
 const Index: React.FC = () => {
-	return <div />;
+	const groupItem = (item: any) => {
+		return (
+			<React.Fragment>
+				{item?.map((item: any, index: number) => {
+					return (
+						<div
+							key={index}
+							className={`default-key ${keyDown.find((keyDown) => keyDown === item.key) ? 'activeDown' : ''} ${
+								historyDown.find((keyDown) => keyDown === item.key) ? 'activeHistory' : ''
+							}`}
+							style={{ width: item.width }}>
+							{item.title}
+						</div>
+					);
+				})}
+			</React.Fragment>
+		);
+	};
+
+	const [keyDown, setKeyDown] = React.useState<string[]>([]);
+	const [historyDown, setHistoryDown] = React.useState<string[]>([]);
+
+	React.useEffect(() => {
+		const state: string[] = [];
+		const history: string[] = [];
+
+		const handleUserKeyDown = (event: { keyCode: number }) => {
+			if (state.indexOf(String(event.keyCode)) === -1) {
+				state.push(String(event.keyCode));
+				history.push(String(event.keyCode));
+			}
+
+			setHistoryDown(history);
+			setKeyDown(state);
+		};
+
+		const handleUserKeyUp = (event: { keyCode: number }) => {
+			const key = String(event.keyCode);
+			const index = state.indexOf(key);
+
+			if (index > -1) {
+				state.splice(index, 1);
+
+				setKeyDown(state);
+			}
+		};
+
+		window.addEventListener('keydown', handleUserKeyDown);
+		window.addEventListener('keyup', handleUserKeyUp);
+
+		window.addEventListener('keydown', function (e: KeyboardEvent) {
+			if (
+				e.keyCode === 9 ||
+				e.keyCode === 91 ||
+				e.keyCode === 93 ||
+				e.keyCode === 112 ||
+				e.keyCode === 114 ||
+				e.keyCode === 116 ||
+				e.keyCode === 117 ||
+				e.keyCode === 118 ||
+				e.keyCode === 122 ||
+				e.keyCode === 121 ||
+				e.keyCode === 123
+			) {
+				e.preventDefault();
+			}
+		});
+		document.oncontextmenu = function (e) {
+			e.preventDefault();
+			return false;
+		};
+	}, []);
+
+	return (
+		<div className='keyboard-background-default'>
+			{keyboard.map((item, index: number) => {
+				return (
+					<React.Fragment key={index}>
+						<div className='container'>
+							{item.lineOne?.map((item, index: number) => {
+								return (
+									<div key={index} className='group'>
+										{groupItem(item.groupOne)}
+										{groupItem(item.groupTwo)}
+										{groupItem(item.groupThree)}
+										{groupItem(item.groupFour)}
+										{groupItem(item.groupFive)}
+									</div>
+								);
+							})}
+						</div>
+						<div className='container'>
+							{item.lineTwo?.map((item, index: number) => {
+								return (
+									<div key={index} className='group'>
+										{groupItem(item.groupOne)}
+										{groupItem(item.groupTwo)}
+									</div>
+								);
+							})}
+						</div>
+						<div className='container'>
+							{item.lineThree?.map((item, index: number) => {
+								return (
+									<div key={index} className='group'>
+										{groupItem(item.groupOne)}
+										{groupItem(item.groupTwo)}
+									</div>
+								);
+							})}
+						</div>
+						<div className='container'>
+							{item.lineFour?.map((item, index: number) => {
+								return (
+									<div key={index} className='group'>
+										{groupItem(item.groupOne)}
+										{groupItem(item.groupTwo)}
+									</div>
+								);
+							})}
+						</div>
+						<div className='container'>
+							{item.lineFive?.map((item, index: number) => {
+								return (
+									<div key={index} className='group'>
+										{groupItem(item.groupOne)}
+										{groupItem(item.groupTwo)}
+									</div>
+								);
+							})}
+						</div>
+						<div className='container'>
+							{item.lineSix?.map((item, index: number) => {
+								return (
+									<div key={index} className='group'>
+										{groupItem(item.groupOne)}
+										{groupItem(item.groupTwo)}
+									</div>
+								);
+							})}
+						</div>
+					</React.Fragment>
+				);
+			})}
+			<div className='registry'>
+				<div className='title'>Registro de Eventos</div>
+				<div className='container-registry'>
+					<div className='subTitle'>09/04/2023 10:10 - Registro de tecla ( A )</div>
+					<div className='subTitle'>09/04/2023 10:10 - Registro de tecla ( B )</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Index;
---
+++ src/pages/keys.ts
@@ -0,0 +1,161 @@
+export const keyboard = [
+	{
+		lineOne: [
+			{ groupOne: [{ title: 'Esc', key: '27' }] },
+			{
+				groupTwo: [
+					{ title: 'F1', key: '112' },
+					{ title: 'F2', key: '113' },
+					{ title: 'F3', key: '114' },
+					{ title: 'F4', key: '115' },
+				],
+			},
+			{
+				groupThree: [
+					{ title: 'F5', key: '116' },
+					{ title: 'F6', key: '117' },
+					{ title: 'F7', key: '118' },
+					{ title: 'F8', key: '119' },
+				],
+			},
+			{
+				groupFour: [
+					{ title: 'F9', key: '120' },
+					{ title: 'F10', key: '121' },
+					{ title: 'F11', key: '122' },
+					{ title: 'F12', key: '123' },
+				],
+			},
+			{
+				groupFive: [
+					{ title: 'Prtsc', key: '0' },
+					{ title: 'Scrlk', key: '145' },
+					{ title: 'Pause', key: '19' },
+				],
+			},
+		],
+	},
+	{
+		lineTwo: [
+			{
+				groupOne: [
+					{ title: '-', key: '192' },
+					{ title: '1 !', key: '49' },
+					{ title: '2 @', key: '50' },
+					{ title: '3 #', key: '51' },
+					{ title: '4 $', key: '52' },
+					{ title: '5 $', key: '53' },
+					{ title: '6 $', key: '54' },
+					{ title: '7 $', key: '55' },
+					{ title: '8 $', key: '56' },
+					{ title: '9 $', key: '57' },
+					{ title: '0 $', key: '48' },
+					{ title: '- -', key: '189' },
+					{ title: '+ =', key: '187' },
+					{ title: 'Backspace', key: '8', width: '7.6rem' },
+				],
+			},
+			{
+				groupTwo: [
+					{ title: 'Insert', key: '45' },
+					{ title: 'Home', key: '36' },
+					{ title: 'Page Up', key: '33' },
+				],
+			},
+		],
+	},
+	{
+		lineThree: [
+			{
+				groupOne: [
+					{ title: 'Tab', key: '9', width: '4rem' },
+					{ title: 'Q', key: '81' },
+					{ title: 'W', key: '87' },
+					{ title: 'E', key: '69' },
+					{ title: 'R', key: '82' },
+					{ title: 'T', key: '84' },
+					{ title: 'Y', key: '89' },
+					{ title: 'U', key: '85' },
+					{ title: 'I', key: '73' },
+					{ title: 'O', key: '79' },
+					{ title: 'P', key: '80' },
+					{ title: '-', key: '219' },
+					{ title: '[ }', key: '221' },
+					{ title: '<', key: '13', width: '5.6rem' },
+				],
+			},
+			{
+				groupTwo: [
+					{ title: 'Del', key: '46' },
+					{ title: 'End', key: '35' },
+					{ title: 'Page Down', key: '34' },
+				],
+			},
+		],
+	},
+	{
+		lineFour: [
+			{
+				groupOne: [
+					{ title: 'Capslock', key: '20', width: '5.6rem' },
+					{ title: 'A', key: '65' },
+					{ title: 'S', key: '83' },
+					{ title: 'D', key: '68' },
+					{ title: 'F', key: '70' },
+					{ title: 'G', key: '71' },
+					{ title: 'H', key: '72' },
+					{ title: 'J', key: '74' },
+					{ title: 'K', key: '75' },
+					{ title: 'L', key: '76' },
+					{ title: 'Ç', key: '186' },
+					{ title: '~ ^', key: '222' },
+					{ title: '] {', key: '220' },
+					{ title: '<', key: '13', width: '4rem' },
+				],
+			},
+			{
+				groupTwo: [{ title: 'Vazio', width: '8.8rem' }],
+			},
+		],
+	},
+	{
+		lineFive: [
+			{
+				groupOne: [
+					{ title: 'Shift', key: '16', width: '4.8rem' },
+					{ title: '|', key: '226' },
+					{ title: 'Z', key: '90' },
+					{ title: 'X', key: '88' },
+					{ title: 'C', key: '67' },
+					{ title: 'V', key: '86' },
+					{ title: 'B', key: '66' },
+					{ title: 'N', key: '78' },
+					{ title: 'M', key: '77' },
+					{ title: ', <', key: '188' },
+					{ title: '. >', key: '190' },
+					{ title: '; :', key: '191' },
+					{ title: '/ ?', key: '193' },
+					{ title: 'Shift', key: '16', width: '4.8rem' },
+				],
+			},
+			{ groupTwo: [{ title: 'Top' }, { title: 'Top', key: '38' }, { title: 'Top' }] },
+		],
+	},
+	{
+		lineSix: [
+			{
+				groupOne: [
+					{ title: 'Ctrl', key: '17' },
+					{ title: 'Win', key: '91' },
+					{ title: 'Alt', key: '18' },
+					{ title: 'Space', key: '32', width: '28rem' },
+					{ title: 'AltGr', key: '18' },
+					{ title: 'Win', key: '91' },
+					{ title: 'Prnt', key: '93' },
+					{ title: 'Ctrl', key: '17' },
+				],
+			},
+			{ groupTwo: [{ title: 'E' }, { title: 'Baixo' }, { title: 'D' }] },
+		],
+	},
+];
